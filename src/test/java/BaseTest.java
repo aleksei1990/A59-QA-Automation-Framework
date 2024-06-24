a00b3fd8bdfdbafe8d83d445ee53db507881e3e7
aleksei1990 <aleksei.koksharov@testpro.io>
2024-06-24 00:03:25 -0500

    update hw19

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -13,46 +13,32 @@ import java.time.Duration;
 public class BaseTest {
 
     WebDriver driver = null;
-
-    //String url = "https://qa.koel.app/";
+    String url = "https://qa.koel.app/";
 
     @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+    static void setupClass() {WebDriverManager.chromedriver().setup();
     }
-
     @BeforeMethod
     @Parameters({"BaseURL"})
     public void launchBrowser(String baseURL){
         //      Added ChromeOptions argument below to fix websocket error
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        navigateToPage(baseURL);
-
-
-
-
+        url = baseURL;
+        navigateToPage(url);
     }
-
     @AfterMethod
     public void closeBrowser(){
         driver.quit();
     }
-
-
-
-
     protected void enterPassword(String password) {
         WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
         passwordField.clear();
         passwordField.sendKeys(password);
     }
-
     protected void enterEmail(String email) {
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
@@ -61,11 +47,8 @@ public class BaseTest {
     protected void submit() {
         WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
-
     }
-
     protected void navigateToPage(String url) {
-
         driver.get(url);
     }
 }
---
+++ src/test/java/Homework19DeletePlaylist.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19DeletePlaylist extends BaseTest {
+
+    @Test
+
+    public void deletePlayList() throws InterruptedException {
+
+
+        String playListName = "Playlist 78";
+
+        enterEmail("aleksei.koksharov@testpro.io");
+        enterPassword("ak1234!@#$");
+        submit();
+        Thread.sleep(4000);
+
+        //Choose to delete Play list 78
+        WebElement choosePlayList = driver.findElement(By.xpath("//section[@id='playlists']//li//a[text()='Playlist 78']"));
+        choosePlayList.click();
+        Thread.sleep(3000);
+
+        //Delete Play list
+        WebElement deleteBtn = driver.findElement(By.xpath("//button[contains(@class, 'del btn-delete-playlist')]"));
+        deleteBtn.click();
+        Thread.sleep(3000);
+
+        Assert.assertFalse(choosePlayList.isDisplayed());
+
+
+    }
+
+
+
+    }
+
+
+
+
